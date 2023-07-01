cb431d16035f82b2ba8f50457330d286cf910df8
Kandarp08 <121113612+Kandarp08@users.noreply.github.com>
2023-07-01 16:16:03 +0530

    Add files via upload

---
+++ js/progress.js
@@ -0,0 +1,27 @@
+fillWidth(document.getElementsByClassName("c")[0], 90);
+fillWidth(document.getElementsByClassName("python")[0], 70);
+fillWidth(document.getElementsByClassName("java")[0], 40);
+fillWidth(document.getElementsByClassName("cpp")[0], 40);
+
+fillWidth(document.getElementsByClassName("html")[0], 90);
+fillWidth(document.getElementsByClassName("javascript")[0], 80);
+fillWidth(document.getElementsByClassName("node")[0], 60);
+fillWidth(document.getElementsByClassName("firebase")[0], 20);
+fillWidth(document.getElementsByClassName("css")[0], 20);
+fillWidth(document.getElementsByClassName("mysql")[0], 20);
+fillWidth(document.getElementsByClassName("php")[0], 10);
+
+function fillWidth(el, maxWidth)
+{
+    let w = 0;
+    let id = setInterval(progress, 10);
+
+    function progress()
+    {
+        if (w == maxWidth)
+            clearInterval(id);
+
+        el.style.width = w + "%";
+        ++w;
+    }
+}
---
+++ js/timeline.js
@@ -0,0 +1,100 @@
+var path = document.getElementById("path");
+var container = document.getElementById("svg");
+
+var maxWidth = container.clientWidth;
+
+window.addEventListener("scroll", check);
+window.addEventListener("resize", check);
+window.addEventListener("load", check);
+window.addEventListener("resize", adjust);
+
+document.getElementById("info").style.top = (getOffset(container).top + container.clientHeight - getOffset(document.getElementById("main")).top) + "px";
+
+for (let i = 1; i <= 5; ++i)
+{
+	var pointer = document.getElementById("pointer" + i);
+	var timestamp = document.getElementById("timestamp" + i);
+	
+	pointer.style.left = ((maxWidth / 6) * i - 33) + "px";
+	timestamp.style.left = ((maxWidth / 6) * i - 13) + "px";
+	timestamp.style.bottom = "-13px";
+
+	if (i > 1)
+	{
+		pointer.style.top = (-(89 * (i - 1))) + "px";
+		timestamp.style.bottom = (89 * (i - 1) - 13) + "px";
+	}
+}
+
+function adjust()
+{
+	maxWidth = container.clientWidth;
+	path.setAttribute("d", "M0, 89 " + maxWidth + ", 89");
+	
+	for (let i = 1; i <= 5; ++i)
+	{
+		var pointer = document.getElementById("pointer" + i);
+		var timestamp = document.getElementById("timestamp" + i);
+	
+		pointer.style.left = ((maxWidth / 6) * i - 33) + "px";
+		timestamp.style.left = ((maxWidth / 6) * i - 13) + "px";
+		timestamp.style.bottom = "-13px"; 		
+
+		if (i > 1)
+		{
+			pointer.style.top = (-(89 * (i - 1))) + "px";
+			timestamp.style.bottom = (89 * (i - 1) - 13) + "px";
+		}
+	}
+}
+
+function check()
+{
+	let scrollTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;		
+
+	if ((scrollTop + window.innerHeight - 20) > getOffset(document.getElementById("svg")).top)
+	{
+		window.removeEventListener("scroll", check);
+		window.removeEventListener("resize", check);
+		window.removeEventListener("load", check);
+		beginTimeline();
+	}
+}
+
+function getOffset(el) 
+{
+	const rect = el.getBoundingClientRect();
+	return {
+    	left: rect.left + window.scrollX,
+    	top: rect.top + window.scrollY
+  	};
+}
+
+function beginTimeline()
+{
+	var id = null;
+	let pos = 0;
+	
+	id = setInterval(propagate, 10);
+	
+	function propagate()
+	{
+		if (pos == maxWidth)
+			clearInterval(id);
+	
+		path.setAttribute("d", "M0, 89 " + pos + ", 89");
+		++pos;
+
+		let elementId = Math.floor((pos - 3) / (maxWidth / 6));
+		let opacity = Number(document.getElementById("pointer" + elementId).style.opacity);
+		
+		if (opacity == 0)
+			pos += 25;
+		
+		opacity = opacity + 0.01;
+
+        document.getElementById("pointer" + elementId).style.opacity = opacity;
+		document.getElementById("innerDiv" + elementId).style.opacity = opacity;
+		document.getElementById("event" + elementId).style.opacity = opacity;
+	}
+}
